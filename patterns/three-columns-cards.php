87d9068e07a1eeff68d19ad151dde4d8674e0155
Axiel Roque <axiel.roque@outlook.es>
2025-10-17 12:36:13 -0400

    Feat: add “Three Columns Cards” pattern (3 columns on desktop, 1 on mobile)

---
+++ patterns/three-columns-cards.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Title: Three Columns Cards
+ * Slug: telica/three-columns-cards
+ * Categories: featured
+ * Description: Three cards in three columns on desktop and stacked on mobile.
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--30)"><!-- wp:columns {"align":"wide","className":"equal-cards","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns alignwide equal-cards"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"card","backgroundColor":"background"} -->
+<div class="wp-block-group card has-background-background-color has-background"><!-- wp:heading -->
+<h2 class="wp-block-heading"><br>Modern WPress</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. <strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"card","backgroundColor":"background"} -->
+<div class="wp-block-group card has-background-background-color has-background"><!-- wp:heading -->
+<h2 class="wp-block-heading"><br>Modern WPress</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"card","backgroundColor":"background"} -->
+<div class="wp-block-group card has-background-background-color has-background"><!-- wp:heading -->
+<h2 class="wp-block-heading"><br>Modern WPress</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
